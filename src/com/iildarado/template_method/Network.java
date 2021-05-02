273d4443be91927b58379221b4608cebe0359e5e
Алмас Синев <26almassinev@gmail.com>
2021-05-02 12:09:07 +0300

    Template method

---
+++ src/com/iildarado/template_method/Network.java
@@ -0,0 +1,27 @@
+package com.iildarado.template_method;
+
+public abstract class Network {
+    String userName;
+    String password;
+
+    Network() {}
+
+    /**
+     * Публикация данных в любой сети.
+     */
+    public boolean post(String message) {
+        // Проверка данных пользователя перед постом в соцсеть. Каждая сеть для
+        // проверки использует разные методы.
+        if (logIn(this.userName, this.password)) {
+            // Отправка данных.
+            boolean result =  sendData(message.getBytes());
+            logOut();
+            return result;
+        }
+        return false;
+    }
+
+    abstract boolean logIn(String userName, String password);
+    abstract boolean sendData(byte[] data);
+    abstract void logOut();
+}
