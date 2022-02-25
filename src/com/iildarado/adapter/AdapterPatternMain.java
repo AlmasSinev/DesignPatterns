9602abba2a77c8a896759fb6c310e99a9bc72a06
AlmasSinev <26almassinev@gmail.com>
2022-02-25 19:20:45 +0300

    Фабричный метод

--- src/com/iildarado/adapter/AdapterPatternMain.java
+++ src/com/iildarado/adapter/AdapterPatternMain.java
@@ -1,13 +1,13 @@
 package com.iildarado.adapter;
 
 public class AdapterPatternMain {
+
     public static void main(String[] args) {
         PaymentGateway paymentGateway = new PaymentGatewayImpl();
         AdvancedPayGateway advancedPayGateway = new AdvancedPaymentGatewayAdapter(paymentGateway);
         String mobile1 = null;
         String mobile2 = null;
         advancedPayGateway.makePayment(mobile1, mobile2);
-
-        // Комментарий 1
     }
+
 }
--- src/com/iildarado/adapter/AdvancedPaymentGatewayAdapter.java
+++ src/com/iildarado/adapter/AdvancedPaymentGatewayAdapter.java
@@ -5,9 +5,11 @@ import com.iildarado.factory.enteties.Account;
 public class AdvancedPaymentGatewayAdapter implements AdvancedPayGateway{
 
     private PaymentGateway paymentGateway;
+
     public AdvancedPaymentGatewayAdapter(PaymentGateway paymentGateway) {
         this.paymentGateway = paymentGateway;
     }
+
     public void makePayment(String mobile1, String mobile2) {
         // получаем номер счета по номеру мобильного телефона
         Account account1 = null;
